e07c8fb1f3c832322eecc9b61ef7f9cb893cf624
andersonbui <andersonbuitron@unicauca.edu.co>
2018-11-10 09:34:45 -0500

    version uno siete

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ import json
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Esta es una prueba de actualizacion 1.6 - Hola a todos esta es la version uno.seis!')
+        self.response.write('Esta es una prueba de actualizacion 1.7 - Hola a todos esta es la version uno.siete!')
 
 class AdminPage(webapp2.RequestHandler):
     def get(self):
